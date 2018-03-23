aa6af1d759c1271cb54fb535db4c2b75f9523fcd
Tony Simoes <sr.tony@gmail.com>
2018-03-23 16:36:26 +0100

    Ajout de dossier ViewModels. Navigation dans l'app.

---
+++ PdfCreator/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace PdfCreator.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
